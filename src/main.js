487b5569438a5a1a478f98659a8a32dd61ee3048
Ray Villalobos <ray@planetoftheweb.com>
2019-02-04 09:37:45 -0800

    Added modules and modified main.js file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "bootstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css/animate.css";
+
+import {
+  faShoppingCart,
+  faDollarSign
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faShoppingCart, faDollarSign);
 
 Vue.config.productionTip = false;
 
