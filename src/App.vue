6960668f414622c3f5e16985600fb19790d0817d
Ray Villalobos <ray@planetoftheweb.com>
2019-02-04 10:57:01 -0800

    Price Component

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
     <h1>My Shop</h1>
     <p class="animated fadeInRight">Take a look at our offerings below</p>
     <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <price value="4.52"></price>
   </div>
 </template>
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import Price from "./components/Price.vue";
 
 export default {
   name: "app",
   components: {
-    FontAwesomeIcon
+    FontAwesomeIcon,
+    Price
   }
 };
 </script>
---
+++ src/components/Price.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>{{ this.prefix + Number.parseFloat(this.value * this.conversion).toFixed(this.precision) }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Number,
+    prefix: {
+      type: String,
+      default: "$"
+    },
+    precision: {
+      type: Number,
+      default: 2
+    },
+    conversion: {
+      type: Number,
+      default: 1
+    }
+  }
+};
+</script>
